ea019f0fd400e7853dab6e34d9e2ca5d22e2ed13
yitingliu <yiting.liu@sia-partners.com>
2024-08-01 10:31:38 -0400

    adding components and work on the nav bar

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and share AI prompts"
+}
+const RootLayout = ({children})=>{
+    return (
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+            <main className='app'>
+                <Nav />
+                {children}
+            </main>
+        </body>
+        </html>
+    )
+}
+
+export default RootLayout
