1c5bfc70b3f578453710479fb68977c99ddf2e79
yitingliu <yiting.liu@sia-partners.com>
2024-08-01 11:45:25 -0400

    adding mongo db and google credentials for users and database

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,12 +9,8 @@ export const metadata = {
 const RootLayout = ({children})=>{
     return (
         <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Document</title>
-        </head>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient' />
             </div>
@@ -22,6 +18,7 @@ const RootLayout = ({children})=>{
                 <Nav />
                 {children}
             </main>
+        </Provider>
         </body>
         </html>
     )
---
+++ components/Provider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SessionProvider } from 'next-auth/react'
+const Provider = ({children, session}) => {
+  return (
+    <div>
+      <SessionProvider session={session}>
+        {children}
+      </SessionProvider>
+    </div>
+  )
+}
+
+export default Provider
