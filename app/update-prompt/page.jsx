f3261494ceb96fd1d1ddb287208affcbef5c7ab9
yitingliu <yiting.liu@sia-partners.com>
2024-08-05 17:06:40 -0400

    update the prompt page

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -27,6 +27,35 @@ const EditPrompt = () => {
         if (promptId) getPromptDetails();
       }, [promptId]);
     
+      const updatePrompt = async (e)=>{
+        e.preventDefault();
+        setSubmitting(true);
+
+
+        if(!promptId) return alert('Prompt ID not found');
+
+        try{
+          const response = await fetch (`/api/prompt/new`, {
+          method: 'PATCH',
+          body: 
+          JSON.stringify({
+            prompt: post.prompt,
+            tag:post.tag
+          })
+          })
+          
+          if(response.ok){
+            router.push('/');
+          }
+
+        }catch(error){
+          console.log(error);
+
+        }finally{
+          setSubmitting(false);
+        }
+      }
+      
   return (
     <Form type="Edit"
     post={post}
